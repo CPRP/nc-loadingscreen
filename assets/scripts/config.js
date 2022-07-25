0898fc0794c684770e170eea86585d5eeab2aa03
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-07-24 22:19:39 -0400

    updated

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,12 +1,12 @@
 Config = {}; // Don't touch
 
-Config.ServerIP = "YOUR IP SERVER";
+Config.ServerIP = "173.208.150.186";
 
 // Social media buttons on the left side
 Config.Socials = [
-    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/cKt4Mpd2PQ"},
-    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "#"},
-    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "#"},
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/nnvz7aV8c4"},
+    {name: "tiktok", label: "tiktok", description: "A Tiktok page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://www.tiktok.com/@criminalsparadiserp"},
+    {name: "patreon", label: "Donations", description: "For donations, feel free to look at the room - #Donations-to-the-server at Discord.", icon: "assets/media/icons/tebex.png", link: "https://www.patreon.com/criminalsparadiserp"},
 ];
 
 Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
@@ -14,8 +14,11 @@ Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in 
 
 // Staff list
 Config.Staff = [
-    {name: "NaorNC #1", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
-    {name: "Owner #2", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
+    {name: "Doug Heffernan", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "George Griffin", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Coltron Scott", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Zeke Price", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Brandin Martinez", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
 
 
 ];
