940733f2bf4092467ee2d675374f0fed46a88712
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-12-01 19:15:11 -0500

    updated

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -5,7 +5,7 @@ Config.ServerIP = "173.208.150.186";
 // Social media buttons on the left side
 Config.Socials = [
     {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/nnvz7aV8c4"},
-    {name: "tiktok", label: "tiktok", description: "A Tiktok page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://www.tiktok.com/@criminalsparadiserp"},
+    {name: "tiktok", label: "Tiktok", description: "A Tiktok page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://www.tiktok.com/@criminalsparadiserp"},
     {name: "patreon", label: "Donations", description: "For donations, feel free to look at the channel - #Donations-to-the-server at Discord.", icon: "assets/media/icons/tebex.png", link: "https://www.patreon.com/criminalsparadiserp"},
 ];
 
@@ -14,15 +14,12 @@ Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in 
 
 // Staff list
 Config.Staff = [
-    {name: "Doug Heffernan", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "George Griffin", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Coltron Scott", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Zeke Price", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Brandin Martinez", description: "Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Ellie Martinez", description: "Admin & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Firoza Green", description: "Moderator", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Jack Wood", description: "Moderator", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
-    {name: "Scorpion King", description: "Moderator", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Jordan", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "George", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Coltron", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Zak", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Ellie", description: "Admin & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
+    {name: "Fire", description: "Moderator", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/971947635692937236/978844525692854272/unknown.png"},
 
 
 ];
